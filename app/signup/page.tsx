0346a6eed8ae4ffed3f248d9ffb51f6665f99219
Nest-Ed <preet119.pk@gmail.com>
2025-07-29 14:16:54 +1200

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,1 +1,10 @@
 // Signup page using Supabase
+
+export default function SignupPage() {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      {/* Add your Supabase signup form here */}
+    </div>
+  );
+}
